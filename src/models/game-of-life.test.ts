a95eac93701d1c74190d2bee31caab82fde0a8d5
Phillip Vose <webmaster@loki7.co.uk>
2023-05-02 16:10:57 +0100

    Updated with first pass

--- src/models/game-of-life.test.ts
+++ src/models/game-of-life.test.ts
@@ -4,4 +4,30 @@ describe("evolve", () => {
   it("should not do anything with an empty grid", () => {
     expect(evolve(new Set<Cell>())).toEqual(new Set<Cell>());
   });
+
+  it("should handle underpopulation when one cell has two neighbours", () => {
+    const underPopulatedCells = new Set<Cell>(["1,1", "1,2", "1,3"]);
+
+    expect(evolve(underPopulatedCells)).toEqual(new Set<Cell>(["1,2"]));
+  });
+
+  it("should handle underpopulation when no cells have enough neighbours", () => {
+    const underPopulatedCells = new Set<Cell>(["1,1", "1,2"]);
+
+    expect(evolve(underPopulatedCells)).toEqual(new Set<Cell>());
+  });
+
+  it("should have result of 1,1", () => {
+    const underPopulatedCells = new Set<Cell>([
+      "1,1",
+      "1,2",
+      "2,2",
+      "2,3",
+      "1,3",
+    ]);
+
+    expect(evolve(underPopulatedCells)).toEqual(
+      new Set<Cell>(["1,1", "2,3", "1,3"])
+    );
+  });
 });
--- src/models/game-of-life.ts
+++ src/models/game-of-life.ts
@@ -1,9 +1,63 @@
 export type Cell = string;
 
 export function generateInitialState() {
-  return new Set<Cell>(["1,1", "1,2", "1,3"]);
+  return new Set<Cell>(["1,1", "1,2", "2,2", "2,3", "1,3"]);
 }
 
 export function evolve(cells: Set<Cell>): Set<Cell> {
-  return cells;
+  const newCells = new Set(cells);
+
+  cells.forEach((cell: Cell) => {
+    // Create array of dead cells surrounding live cells
+    // Loops over those to identify those that have three neighbours that a alive
+    // store that result
+
+    const [cellRow, cellColumn] = cell.split(",");
+
+    let numberOfAjecentCells: number = 0;
+
+    if (cells.has(`${parseInt(cellRow) + 1},${cellColumn}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${cellRow},${parseInt(cellColumn) + 1}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${parseInt(cellRow) + 1},${parseInt(cellColumn) + 1}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${parseInt(cellRow) - 1},${cellColumn}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${cellRow},${parseInt(cellColumn) - 1}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${parseInt(cellRow) - 1},${parseInt(cellColumn) - 1}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${parseInt(cellRow) + 1},${parseInt(cellColumn) - 1}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (cells.has(`${parseInt(cellRow) - 1},${parseInt(cellColumn) + 1}`)) {
+      numberOfAjecentCells++;
+    }
+
+    if (numberOfAjecentCells < 2) {
+      newCells.delete(cell);
+    }
+
+    if (numberOfAjecentCells > 3) {
+      newCells.delete(cell);
+    }
+  });
+
+  // merge two arrays before returning
+
+  return newCells;
 }
